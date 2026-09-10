e7a332956d403cdf9bc956f6b3259098e1d14593
Vipul Kumar <vipulk19778@gmail.com>
2026-09-10 13:55:43 +0530

    feat: Simplify Gmail SMTP configuration by removing unnecessary options

--- config/email.config.js
+++ config/email.config.js
@@ -1,14 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  family: 4,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 20000,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
